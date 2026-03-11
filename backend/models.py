126c69e3d4e308396bcbce820219b964cf41732b
thedann3r <austindan168@email.com>
2026-03-11 14:27:26 +0300

    first commit

---
+++ backend/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable = False)
+    content = db.Column(db.Text, nullable = True)
+    created_at = db.Column(db.DateTime, default = db.func.current_timestamp())
