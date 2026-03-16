940203982342f9f5f8241f0cc7ee3517827cbfe9
thedann3r <austindan168@email.com>
2026-03-16 09:54:20 +0300

    notes post

--- backend/resources/crud.py
+++ backend/resources/crud.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from models import Notes
+from models import db, Notes
 from flask_restful import Resource, Api
 
 # app = Flask(__name__)
@@ -8,4 +8,16 @@ from flask_restful import Resource, Api
 class Note(Resource):
     def get(self):
         notes = Notes.query.all()
-        return[note.to_dict() for note in notes], 200
+        return [note.to_dict() for note in notes], 200
+    
+    def post(self):
+        data = request.get_json()
+
+        new_note = Notes(
+            title = data.get("title"),
+            content = data["content"]
+        )
+        db.session.add(new_note)
+        db.session.commit()
+
+        return new_note.to_dict(), 201
