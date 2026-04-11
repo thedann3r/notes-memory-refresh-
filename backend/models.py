5d2693f19d11af5cbce9468c45ddb2b16ca7be3b
thedann3r <austindan168@email.com>
2026-04-11 16:17:23 +0300

    define relationship and serialize rules

--- backend/models.py
+++ backend/models.py
@@ -3,13 +3,6 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Notes(db.Model, SerializerMixin):
-    __tablename__ = "notes"
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(50), nullable = False)
-    content = db.Column(db.Text, nullable = True)
-    created_at = db.Column(db.DateTime, default = db.func.current_timestamp())
-
 class Users(db.Model, SerializerMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
@@ -17,4 +10,22 @@ class Users(db.Model, SerializerMixin):
     email = db.Column(db.String(120), nullable = False, unique = True)
     password = db.Column(db.String(120), nullable = False)
 
-    serialize_rules = ("-password",)
+    notes = db.relationship("Notes", back_populates = "user", cascade = "all, delete orphan")
+
+    serialize_rules = ("-password", "-notes.user")
+
+class Notes(db.Model, SerializerMixin):
+    __tablename__ = "notes"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable = False)
+    content = db.Column(db.Text, nullable = True)
+    created_at = db.Column(db.DateTime, default = db.func.current_timestamp())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+
+    user = db.relationship("Users", back_populates = "notes")
+
+    serialize_rules = ("-user.notes",)
+
+
+
+
